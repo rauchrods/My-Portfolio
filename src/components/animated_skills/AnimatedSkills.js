9e18650a2d6d7a6068825a5275e2ec34c9a3a59e
rauchrods <rauchrodrigues@gmail.com>
2025-05-18 23:01:34 +0530

    added framer bounce effect to animated skills

--- src/components/animated_skills/AnimatedSkills.js
+++ src/components/animated_skills/AnimatedSkills.js
@@ -1,29 +1,43 @@
 import React, { useEffect, useState } from "react";
 import styles from "./AnimatedSkills.module.css";
 import { mySkills } from "../../assets/data/myskills";
+import { motion } from "framer-motion";
 
 function AnimatedSkills() {
   const [currentSkill, setCurrentSkill] = useState(0);
+  const [key, setKey] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setCurrentSkill((prevState) =>
         prevState < mySkills.length - 1 ? prevState + 1 : 0
       );
+      setKey((prevKey) => prevKey + 1);
     }, 900);
 
     return () => clearTimeout(timer);
   }, [currentSkill]);
 
   return (
-    <div
+    <motion.div
+      key={key}
+      initial={{ scale: 0.35, opacity: 0.7 }}
+      animate={{
+        scale: 1,
+        opacity: 1,
+        transition: {
+          type: "spring",
+          stiffness: 500,
+          damping: 15,
+        },
+      }}
       className={styles.skills_section}
       style={{ color: mySkills[currentSkill].color }}
     >
       <span>{mySkills[currentSkill].skill}</span>
 
       <span className={styles.svg_container}>{mySkills[currentSkill].svg}</span>
-    </div>
+    </motion.div>
   );
 }
 
